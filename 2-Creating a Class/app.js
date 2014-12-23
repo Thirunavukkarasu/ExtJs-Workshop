c7ed7980e2012e809aa695f0442e757e669bc856
Thirunavukkarasu Muthuswamy <tamilan.arasu@gmail.com>
2014-12-23 14:52:23 +0530

    Update app.js

--- 2-Creating a Class/app.js
+++ 2-Creating a Class/app.js
@@ -2,7 +2,7 @@ Ext.application({
     name : 'extjs-development',
     launch : function(){
         
- /*       Ext.define('Runner', 
+    Ext.define('Runner', 
         {
             name : 'Usian Bolt',
             getRunnerName : function(){
@@ -13,8 +13,6 @@ Ext.application({
         });
         
         var runnerObject = Ext.create('Runner');
-        console.log(runnerObject.getRunnerName());*/
-        
-        alert("Workspace Loaded successfully!")
+        console.log(runnerObject.getRunnerName());
     }
 });
